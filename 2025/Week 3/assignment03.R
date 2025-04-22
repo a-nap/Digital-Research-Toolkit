a825d4028ab2fc675018bf6fac6a1aa2d37e03e9
Anna Prysłopska <anna.pryslopska@gmail.com>
2025-04-22 15:27:04 +0200

    Added homework assignment for week 3

---
+++ 2025/Week 3/assignment03.R
@@ -0,0 +1,36 @@
+# Homework assignment week 3
+# AUTOGRADER INFORMATION/CAUTION
+# Type your answers in the comments next to the word "ANSWER". 
+# Do not make new lines, delete code, or change the code. This might make the autograder fail.
+
+# 1. According to R, what is the type of the following variables:
+"R"               # ANSWER
+-10               # ANSWER
+FALSE             # ANSWER
+3.14              # ANSWER
+as.logical(1)     # ANSWER
+as.numeric(TRUE)  # ANSWER
+
+# 2. According to R, are the following two variables equivalent (yes/no):
+7+0i == 7         # ANSWER
+9 == 9.0          # ANSWER
+"zero" == 0L      # ANSWER
+"cat" == "cat"    # ANSWER
+TRUE == 1         # ANSWER
+
+# 3. What is the output of the following operations? If there is an error, what caused it?
+-10 > 1           # ANSWER
+5 != 4            # ANSWER
+5 - FALSE         # ANSWER
+17.0 == 7         # ANSWER
+4 = 9.1           # ANSWER
+0/0               # ANSWER
+"toolkit" + 1     # ANSWER
+toolkit = 2       # ANSWER
+toolkit * 2       # ANSWER
+(1-2)/0           # ANSWER
+10 -> 20          # ANSWER
+NA == NA          # ANSWER
+NA == Inf         # ANSWER
+
+# 4. Create a long text (30-50 words) and save it to a variable called "long_text".
---
+++ 2025/Week 3/assignment03.md
@@ -0,0 +1,7 @@
+# Assignment 03
+
+Upload 1 file to complete this assignment: assignment03.R
+
+## AUTOGRADER INFORMATION/CAUTION
+
+Type your answers in the comments next to the word "ANSWER" for tasks 1-3.
