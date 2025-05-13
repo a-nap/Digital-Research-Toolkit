aadfda397d755b03ecce6ea9f9bc0c61bfde5c46
Anna Prysłopska <anna.pryslopska@gmail.com>
2025-05-13 13:40:35 +0200

    Added files for week 5

---
+++ 2025/Week 5/assignment05.R
@@ -0,0 +1,62 @@
+###########################################################################
+# Assignment Week 5
+###########################################################################
+
+# Please complete the following 3 tasks. Submit the assignment as a single R script. 
+# Use comments and sections to give your file structure. I should be able to run
+# your script without errors.
+
+
+# Task 1 ------------------------------------------------------------------
+# Using pipes, clean up the original, Moses illusion raw data like we did in 
+# the tasks in class and save it to a new data frame. 
+# - select relevant columns
+# - rename mislabeled columns
+# - remove missing data
+# - remove unnecessary rows
+# - arrange by condition, and answer
+# - re-code the ITEM column as a number
+# For this task 1, use the original, not preprocessed data: moses_raw_data.csv.
+
+
+
+
+
+
+
+
+
+# Task 2 ------------------------------------------------------------------
+# Using if-else or case_when statements, change the CONDITION column to have 
+# more descriptive names for conditions instead of numbers. 
+# Condition 1 are the Moses illusion questions.
+# Condition 2 are control questions, which have a single, predefined correct answer.
+# Condition 100 are good filler questions, which have a single, predefined correct answer.
+# Condition 101 are bad filler questions, which have no correct answer.
+# For this task, use the preprocessed data and questions: moses_clean.csv and questions.csv
+# If you save the result of this exercise as a new variable, you can use it for 
+# the next next exercise.
+
+
+
+
+
+# Task 3 ------------------------------------------------------------------
+# Calculate the percentage of "correct", "incorrect", and "don't know" answers 
+# in the two critical conditions (think about which conditions these are).
+# Include the code for answering these questions.
+# For this task, use the preprocessed data and questions: moses_clean.csv and questions.csv
+# (i.e. the data frame you )
+
+# Task 3A -----------------------------------------------------------------
+# Of all the questions in all conditions, which question was the easiest and 
+# which was the hardest?
+
+  
+# Task 3B ------------------------------------------------------------------
+# Of the Moses illusion questions, which question fooled most people?
+
+
+# Task 3C ------------------------------------------------------------------
+# Which participant was the best in answering questions? Who was the worst?
+  
---
+++ 2025/Week 5/assignment05.md
@@ -0,0 +1,3 @@
+# Assignment 05
+
+Complete and upload the file assignment05.R Remember to rename your file with your name and assignment number.
